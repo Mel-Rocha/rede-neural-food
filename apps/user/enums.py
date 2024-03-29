11096c58cb97e11c03a4cc006bf866fd8c2f457e
Mel-Rocha <rochamel73@gmail.com>
2024-03-29 13:05:10 -0300

    first commit

---
+++ apps/user/enums.py
@@ -0,0 +1,19 @@
+GOALS = [
+        ('hypertrophy', 'Hypertrophy'),
+        ('maintenance', 'Maintenance'),
+        ('weight_loss', 'Weight Loss'),
+    ]
+
+
+DIET = [
+    ('vegetarian', 'Vegetarian'),
+    ('ornivorous', 'Ornivorous'),
+    ('paleolithic', 'Paleolithic'),
+    ('mediterranean', 'Mediterranean'),
+    ('ketogenic', ' ketogenic'),
+]
+
+GENDER = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ]
