86514772bcf3d7b56fb71d0dfc6df8062c2d5853
Mel-Rocha <rochamel73@gmail.com>
2024-03-30 02:05:31 -0300

    opção de criar perfil, preciso ajustar o redirecionamento de login

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
 from . import views
+from apps.home.views import CustomLoginView
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 
-# Create your views here.
 
 def index(request):
 
     # Page from the theme 
     return render(request, 'pages/index.html')
+
+
+class CustomLoginView(LoginView):
+    def get_success_url(self):
+        if self.request.user.is_authenticated:
+            if self.request.user.profile:
+                profile_pk = self.request.user.profile.pk
+                return reverse_lazy('profile_detail', kwargs={'pk': profile_pk})
+            else:
+                return reverse_lazy('profile_create')
+        else:
+            return reverse_lazy('login')
+
+LOGIN_REDIRECT_URL = CustomLoginView()
